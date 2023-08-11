1947bc701f4e1d75f5527580f6e4fd813e15e03e
fatemeh <mmadinohggfgj@gmail.com>
2023-08-11 14:49:14 +0330

    my js tamrin

---
+++ 04.js
@@ -0,0 +1,31 @@
+// function tabdil(){
+//   let c=+prompt("cilcuc ra vard kon")
+//   const  f=c*9/5+32
+//     return f
+// }
+// console.log(tabdil())
+//++++++++++++++++++++++++++++++++++++++++++++++
+// function serch(){
+//     let jomle=prompt("jomle")
+//     let kalame=prompt("kalameh")
+//     if(jomle.includes(kalame)){
+//         return true
+
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(serch())
+//++++++++++++++++++++++++++++++++++++++++++++++
+// let obj={
+//     name:"laptob",
+//     ghaymat:650000,
+//     add(){
+
+//     },
+//     del(){
+
+//    }
+// }
+
