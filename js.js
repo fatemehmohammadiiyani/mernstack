a45fd3475d696447c002b80f8fd117eb974bc5e3
fatemeh <mmadinohggfgj@gmail.com>
2023-08-19 18:53:19 +0330

    fixed

---
+++ js.js
@@ -0,0 +1,50 @@
+// function clac(){
+//     let firstnumber=+prompt("firstnumber")
+//     let oorator=prompt("oorator")
+//     let secendnumber=+prompt("secendnumber")
+//     switch(oorator){
+// case"-":
+// return firstnumber-secendnumber
+
+// case"+":
+// return firstnumber+secendnumber
+
+// case"*":
+// return firstnumber-secendnumber
+
+// case"/":
+// return firstnumber+secendnumber
+
+//     }
+// }
+// console.log(clac())
+
+// function caps(jomle=""){
+//    return  jomle.toUpperCase()
+// }
+// console.log(caps(prompt("enter your sentens:")))
+
+// function moadel(){
+// let n1=+prompt("1")
+// let n2=+prompt("2")
+// let n3=+prompt("3")
+// let n4=+prompt("4")
+// let n5=+prompt("5")
+//  let jame=n1+n2+n3+n4+n5/5
+// return jame
+// }
+// console.log(moadel())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
